b6a507235ba7889701c851686d8e8f6de1e53161
OlesyaIv <fortuna.o@mail.ru>
2018-01-25 18:43:23 +0300

    Update JsonValidator.java

--- src/main/java/JsonValidator.java
+++ src/main/java/JsonValidator.java
@@ -1,91 +1,89 @@
 import com.google.gson.*;
 import com.sun.net.httpserver.HttpServer;
-import net.sf.oval.constraint.NotNull;
+import org.jetbrains.annotations.NotNull;
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.util.stream.Collectors;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//import java.io.BufferedReader;
 
-public class json_validate {
+/*
+Create server and check json file
+*/
 
-        public json_validate(HttpServer server) throws IOException {
+public class JsonValidator {
+    public JsonValidator(HttpServer server) throws IOException {
+	    
+	    Gson builder = new GsonBuilder().setPrettyPrinting().create();
+	    server.bind(new InetSocketAddress(80), 0);
+	    
+	    server.createContext("/", http -> {
+		    /* server start */
+		    
+		    /*Read text from stream*/
+		    InputStreamReader isr = new InputStreamReader(http.getRequestBody());
+		    final String jsonRequest = new BufferedReader(isr).lines().collect(Collectors.joining());
+		    System.out.println("request:" + jsonRequest);
+		    
+		    /*String for result*/
+		    
+		    String jsonResponse;
+		    String respStr = null;
+		    
+		    /*Now try convert to string and see errors*/
+		    try {
+		
+			    Object object = builder.fromJson(jsonRequest, Object.class);
+			    jsonResponse = builder.toJson(object);   
+		    } 
 
-                Gson gson = new GsonBuilder().setPrettyPrinting().create();
-                server.bind(new InetSocketAddress(80), 0);
-                server.createContext("/", httpExchange -> {
-
-                        int id = 0;
- 
-                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpExchange.getRequestBody()));
-                        String buildString = bufferedReader.readLine();
-                        StringBuilder stringBuilder = new StringBuilder();
-                        while (buildString != null) {
-                                stringBuilder.append(buildString);
-                                buildString = bufferedReader.readLine();
-                        }
-                        String request = stringBuilder.toString();
-                        System.out.println("Request: " + request);
-                        String response = null;
-
-                        try {
-
-                                Object object = gson.fromJson(request, Object.class);
-                                response = gson.toJson(object);
-
-                        } catch (JsonSyntaxException ex) {
- 					String exception = ex.toString();
-           				 String[] er = exception.split(" at ");
-            				String errorMessage = er[0]; 
-            				String errorPlace = er[1]; 
-            				String id = Integer.toString(ex.hashCode());
-                                int errorCode = exception.hashCode();
-            
-           				 errStr = "{\n" +
-            					" \"errorCode\"  : 12345,\n" +
-            					" \"errorMessage\" : \"" + errorMessage + "\",\n" +
-            					" \"errorPlace\" : \"" + errorPlace + "\",\n" +
-            					" \"resource\"   : \"" + file + "\",\n" +
-            					" \"request-id\" : \"" + id + "\",\n" +
-            					"}";
-        					}
-        			return errStr;
-//*
-                                JsonObject error = new JsonObject();
- 
-                                String errorDescription = exception.getMessage().split(": ")[1];
-                                String errorMessage = errorDescription.split(" at ")[0]; 
-                                String errorPlace = errorDescription.split(" at ")[1];
-   
-                                int errorCode = exception.hashCode();
-  
-                                error.addProperty("errorCode", errorCode);
-                                error.addProperty("errorMessage", errorMessage);
-                                error.addProperty("errorPlace", errorPlace);
-                                error.addProperty("resource", request);
-                                error.addProperty("request-id", id);
-                                response = gson.toJson(error);
-                        }
-                        id = id + 1;
-
-                        System.out.println(response);
-
-                        httpExchange.sendResponseHeaders(200, response.length());
-                        httpExchange.getResponseBody().write(response.getBytes());
-                        httpExchange.close();
-*//
-                });
-        }
-
-
-        public static void main(String[] args) throws IOException {
-
+		    catch (JsonSyntaxException ex) {
+			    
+		    /*Now create erorr-message*/
+			    JsonObject jsonError = new JsonObject();
+			    jsonError.addProperty("message", ex.getMessage());
+			    jsonResponse = builder.toJson(jsonError);
+			    
+		    /*Now create response*/
+			    String exception = ex.toString();
+			    String[] er = exception.split(" at ");
+			    String errorMessage = er[0]; 
+			    String errorPlace = er[1]; 
+			    String id = Integer.toString(ex.hashCode());
+			    
+			    respStr = "{\n" +
+				    " \"errorCode\"  : 12345,\n" +
+				    " \"errorMessage\" : \"" + errorMessage + "\",\n" +
+				    " \"errorPlace\" : \"" + errorPlace + "\",\n" +
+				    " \"resource\"   : \"" + jsonRequest + "\",\n" +
+				    " \"request-id\" : \"" + id + "\",\n" +
+				    "}";
+		    }
+		    
+		    /*And now we can see our result*/
+		    System.out.println("response:" + respStr);
+		    http.sendResponseHeaders(200, respStr.length());
+		    http.getResponseBody().write(respStr.getBytes());
+		    
+		    /*close server*/
+		    http.close();
+		    
+	    });
+    }
+	
+    /*Starting server and waiting for a Json files*/	
+    public static void main(String[] args) throws IOException {
                 final HttpServer server = HttpServer.create();
-                json_validate json = new json_validate(server);
+                JsonValidator json = new JsonValidator(server);
                 json.start(server);
-        }
+    }
+	
 
-
+	/*Function for start and stop server*/
+	
         private static void start(HttpServer server){server.start();}
-
-         private static void stop(HttpServer server){server.stop(0);}
-
+        private static void stop(HttpServer server){server.stop(0);}
+	
+	/*End function for start and stop server*/
 }
