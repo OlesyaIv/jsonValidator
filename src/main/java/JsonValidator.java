3c4ab47bb90dadf2c90681d9eae8f54a1eb8ceda
OlesyaIv <fortuna.o@mail.ru>
2018-01-31 16:09:42 +0300

    Update JsonValidator.java

--- src/main/java/JsonValidator.java
+++ src/main/java/JsonValidator.java
@@ -72,14 +72,14 @@ public class JsonValidator {
 			    String[] er = exception.split(" at ");
 			    String errorMessage = er[0]; 
 			    String errorPlace = er[1]; 
-			    String id = Integer.toString(ex.hashCode());
+			  //  String id = Integer.toString(ex.hashCode());
 			 
 			    respStr = "{\n" +
 				    " \"errorCode\"  : 12345,\n" +
 				    " \"errorMessage\" : \"" + errorMessage + "\",\n" +
 				    " \"errorPlace\" : \"" + errorPlace + "\",\n" +
 				    " \"resource\"   : \"" + filename + "\",\n" +
-				    " \"request-id\" : \"" + id + "\",\n" +
+				    " \"request-id\" : \"" + cnt + "\",\n" +
 				    "}";
 		    }
 		    
